83a97e8bb217a6c5d0117b2277ab9ef21450c6f5
Tran Hai Dang <tranhaidang2320@gmail.com>
2022-07-02 18:26:06 +0700

    indexOf implement

--- src/main/scala/leetcode/easy/ImplementstrStr.scala
+++ src/main/scala/leetcode/easy/ImplementstrStr.scala
@@ -3,4 +3,22 @@ package leetcode.easy
 
 object ImplementstrStr extends App {
 
+  def strStr(haystack: String, needle: String): Int = {
+    if (needle.isEmpty) 0
+    else if (needle.length > haystack.length) -1
+    else indexOf(needle, haystack, 0)
+
+  }
+
+  def compareSub(needle: String, haystack: String, index: Int): Boolean = {
+    needle == haystack.substring(index, index + needle.length)
+  }
+
+  def indexOf(needle: String, haystack: String, index: Int): Int = {
+    if (index > haystack.length - needle.length) -1
+    else if (compareSub(needle, haystack, index)) index
+    else indexOf(needle, haystack, index + 1)
+  }
+
+  println(strStr("aaaaa", "bbaz"))
 }
